fd9c856633dafbf08aa453e85dfc5891326156de
danialHaghmoradi <daniyal1384422@gmail.com>
2025-02-19 15:05:41 +0330

    add other page in build

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,13 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
   base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "/index.html",
+        about: "/about.html",
+        pricing: "/pricing.html",
+      },
+    },
+  },
 });
