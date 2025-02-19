fd83ba239a9a909a61fa83c070b08d88b45de60e
danialHaghmoradi <daniyal1384422@gmail.com>
2025-02-19 15:33:16 +0330

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,4 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: "/index.html",
-        about: "/about.html",
-        pricing: "/pricing.html",
-      },
-    },
-  },
 });
